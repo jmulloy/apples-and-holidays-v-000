4b2e95184c0a32fc1ca70551bf417bb6f1b848b6
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-20 15:39:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,8 +36,7 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-def add_supply_to_memorial_day(holiday_supplies)
-  holiday_supplies[:summer][:fourth_of_july][1]
+  holiday_hash[:spring][:memorial_day].push(supply)
 end
 
 end
